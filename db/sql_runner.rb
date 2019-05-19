322e40768f579a1ca0fc8a0ce31ee2e94266d758
user <gibson.stephen92@gmail.com>
2019-05-19 16:49:13 +0100

    added env variables

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,13 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'pet_pro', host: 'localhost' })
+      db = PG.connect({ 
+        dbname: ENV['DBNAME'],
+        host: ENV['DBHOST'],
+        port:  ENV['DBPORT'],
+        user: ENV['DBUSER'],
+        password: ENV['DBPASSWORD']
+      })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
