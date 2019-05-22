504a4227f10605630f061c0d2a265dd761014cdd
Amy Edwardson <am4zed11@gmail.com>
2019-05-22 21:50:05 +0100

    further changes for heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -6,21 +6,21 @@ class SqlRunner
   def self.run( sql, values = [] )
     begin
       db = PG.connect({
-        dbname: ENV['d4trtfh5g4qb8r'],
-        host: ENV['ec2-174-129-242-183.compute-1.amazonaws.com'],
-        port:  ENV[5432],
-        user: ENV['bmehnvrreifrxj'],
-        password: ENV['74f07137bafe772f5985554c8321a1d66cfdd03c56dd311c44a9bb319ec1f9c1']
-        })
-        db.prepare("query", sql)
-        result = db.exec_prepared( "query", values )
-      ensure
-        db.close() if db != nil
-      end
-      return result
+        dbname: ENV['DBNAME'],
+        host: ENV['DBHOST'],
+        port:  ENV['DBPORT'],
+        user: ENV['DBUSER'],
+        password: ENV['DBPASSWORD']
+      })
+      db.prepare("query", sql)
+      result = db.exec_prepared( "query", values )
+    ensure
+      db.close() if db != nil
     end
-
+    return result
   end
+
+end
 
 
   # LOCALHOST SETTINGS
